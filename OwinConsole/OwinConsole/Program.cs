411a949e5e9174f9e0fc3bd39a33b73151d6afbb
Amar Kumar Dubedy <amdubedy@microsoft.com>
2016-11-08 07:57:06 -0800

    First working owin project

---
+++ OwinConsole/OwinConsole/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinConsole
+{
+    using System.IO;
+    using Microsoft.Owin.Hosting;
+    using Owin;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started !!");
+                Console.ReadKey();
+                Console.WriteLine("Stopped !!");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // To add a default welcome page
+            // app.UseWelcomePage();
+
+            app.UseHelloWorld();
+        }
+    }
+
+    // To allow for syntactic sugar
+    public static class AppBuilderExtensions
+    {
+        public static void UseHelloWorld(this IAppBuilder app)
+        {
+            // This code can directly be used in startup configuration
+            app.Use<HelloWorldComponent>();
+        }
+    }
+
+    // Owin component
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            this._next = next;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello World !");
+            }
+        }
+    }
+}
+
