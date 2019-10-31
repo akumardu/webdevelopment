7a12f392e533f08da0b2f52df620172983e4f824
Amar Kumar Dubedy <amdubedy@microsoft.com>
2019-10-31 12:11:14 -0700

    Basic setup of Angular app: what is a component, etc

--- LLearning/AngEssentials/src/app/app.component.ts
+++ LLearning/AngEssentials/src/app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: '<h1>My App</h1>'
+    // either use template or template URL
+    // template: '<h1>My App</h1>'
+    templateUrl: './app.component.html',
+    // styles could also be specified via URLs
+    // styles: [`
+    // h1 {color: #ffffff;}
+    // .description {
+    //     font-style: italic;
+    //     color: green;
+    // }
+    // `]
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent{}
