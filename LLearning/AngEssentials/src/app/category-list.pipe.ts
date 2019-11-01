44b9d53ef74fc4605151b189f034c86c36cd9454
Amar Kumar Dubedy <amdubedy@microsoft.com>
2019-10-31 18:34:44 -0700

    Finished Angular directives and pipes

---
+++ LLearning/AngEssentials/src/app/category-list.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'categoryList',
+    pure: true // Indicates if the pipe maintains state
+})
+export class CategoryListPipe implements PipeTransform {
+    transform(mediaItems) {
+        const categories = [];
+        mediaItems.forEach(mediaItem => {
+            if(categories.indexOf(mediaItem.category) <= 1) {
+                categories.push(mediaItem.category);
+            }
+        });
+
+        return categories.join(', ');
+    }
+}
