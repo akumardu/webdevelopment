5df3ffc27e1a3992117c72e9d6f54e761a242189
Amar Kumar Dubedy <amdubedy@microsoft.com>
2019-11-02 07:12:42 -0700

    Using Http to call backend services. Also used Mock XHR backend to mock http service instead of creating one

--- LLearning/AngEssentials/src/app/app.module.ts
+++ LLearning/AngEssentials/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
@@ -9,6 +10,7 @@ import { MediaItemFormComponent } from './media-item-form.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { lookupListToken, lookupLists } from './providers';
+import { MockXHRBackend } from './mock-xhr-backend';
 
 // value providers - these are different from media-item.service providers
 // const lookupLists = {
@@ -19,7 +21,8 @@ import { lookupListToken, lookupLists } from './providers';
     imports: [
         BrowserModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
@@ -33,6 +36,10 @@ import { lookupListToken, lookupLists } from './providers';
         {
             provide: lookupListToken, 
             useValue: lookupLists
+        },
+        {
+            provide: HttpXhrBackend,
+            useClass: MockXHRBackend
         }
     ],
     bootstrap: [
--- LLearning/AngEssentials/src/app/media-item-form.component.ts
+++ LLearning/AngEssentials/src/app/media-item-form.component.ts
@@ -49,7 +49,7 @@ export class MediaItemFormComponent implements OnInit{
   }
 
   onSubmit(mediaItem){
-    this.mediaItemService.add(mediaItem);
+    this.mediaItemService.add(mediaItem).subscribe();
   }
 }
 
